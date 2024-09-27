268514514c4a09d62db0fdc5195332e076405519
JKearnsl <boyarshinovn@gmail.com>
2024-09-27 15:06:59 +0300

    added hasher

---
+++ src/application/common/hasher.rs
@@ -0,0 +1,8 @@
+use async_trait::async_trait;
+
+
+#[async_trait]
+pub trait Hasher {
+    async fn hash(&self, value: &str) -> String;
+    async fn verify(&self, value: &str, hash: &str) -> bool;
+}
