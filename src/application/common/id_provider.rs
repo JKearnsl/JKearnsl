76879a1888daa325f76c2d1468ef29e8394295b7
JKearnsl <boyarshinovn@gmail.com>
2025-01-18 20:34:27 +0300

    update: add tests; rename `token` -> `session`

--- src/application/common/id_provider.rs
+++ src/application/common/id_provider.rs
@@ -1,6 +1,32 @@
 
-pub trait IdProvider {
-    fn token(&self) -> Option<&String>;
+pub trait IdProvider: Send + Sync {
+    fn session(&self) -> Option<&String>;
     fn username(&self) -> Option<&String>;
     fn is_auth(&self) -> &bool;
+}
+
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+
+    pub struct MockIdProvider {
+        pub session: Option<String>,
+        pub username: Option<String>,
+        pub is_auth: bool,
+    }
+
+    impl IdProvider for MockIdProvider {
+        fn session(&self) -> Option<&String> {
+            self.session.as_ref()
+        }
+
+        fn username(&self) -> Option<&String> {
+            self.username.as_ref()
+        }
+
+        fn is_auth(&self) -> &bool {
+            &self.is_auth
+        }
+    }
 }
