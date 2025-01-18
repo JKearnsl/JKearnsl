d3ed2dcef7bfaec2ef5724a48b733230ed118f53
JKearnsl <boyarshinovn@gmail.com>
2025-01-18 20:34:55 +0300

    update: ch hash type to `Hash`; add tests

--- src/application/common/hasher.rs
+++ src/application/common/hasher.rs
@@ -1,8 +1,31 @@
 use async_trait::async_trait;
-
+use crate::domain::models::hash::Hash;
 
 #[async_trait]
 pub trait Hasher {
-    async fn hash(&self, value: &str) -> String;
-    async fn verify(&self, value: &str, hash: &str) -> bool;
+    async fn hash(&self, value: &str) -> Hash;
+    async fn verify(&self, value: &str, hash: &Hash) -> bool;
+}
+
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+
+    pub struct MockHasher;
+
+    #[async_trait]
+    impl Hasher for MockHasher {
+        async fn hash(&self, value: &str) -> Hash {
+            let mut data = [0; Hash::SIZE];
+            let bytes = value.as_bytes();
+            let len = bytes.len().min(Hash::SIZE);
+            data[..len].copy_from_slice(&bytes[..len]);
+            Hash(data)
+        }
+
+        async fn verify(&self, value: &str, hash: &Hash) -> bool {
+            self.hash(value).await == *hash
+        }
+    }
 }
