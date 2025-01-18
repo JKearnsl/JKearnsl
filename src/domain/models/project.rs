e506c59466f0790823d9b7ba8a0d61df9de151ca
JKearnsl <boyarshinovn@gmail.com>
2025-01-18 20:30:29 +0300

    update: impl project logic

--- src/domain/models/project.rs
+++ src/domain/models/project.rs
@@ -1,12 +1,14 @@
+use std::collections::HashMap;
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use crate::domain::id_generator::generate_id;
 
 pub type ProjectId = String;
 
 pub const PROJECT_ID_SIZE: usize = 16;
-pub const PROJECT_TITLE_LENGTH: (usize, usize) = (1, 128);
-pub const PROJECT_DESCRIPTION_LENGTH: (usize, usize) = (1, 256);
-pub const PROJECT_URL_LENGTH_MAX: usize = 2048;
+pub const PROJECT_TITLE_MAX: usize = 128;
+pub const PROJECT_DESCRIPTION_MAX: usize = 256;
+pub const PROJECT_URL_MAX: usize = 2048;
 
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -16,4 +18,45 @@ pub struct Project {
     pub description: String,
     pub url: Option<String>,
     pub created_at: DateTime<Utc>
+}
+
+impl Project {
+    pub fn create(title: String, description: String, url: Option<String>) -> anyhow::Result<Self, HashMap<String, String>> {
+        if title.len() > PROJECT_TITLE_MAX {
+            return Err(HashMap::from([(
+                "title".to_string(), 
+                format!("is too long: {} > {}", title.len(), PROJECT_TITLE_MAX)
+            )]));
+        }
+        
+        if description.len() > PROJECT_DESCRIPTION_MAX {
+            return Err(HashMap::from([(
+                "description".to_string(), 
+                format!("is too long: {} > {}", description.len(), PROJECT_DESCRIPTION_MAX)
+            )]));
+        }
+        
+        if let Some(url) = &url {
+            if url.len() > PROJECT_URL_MAX {
+                return Err(HashMap::from([(
+                    "url".to_string(), 
+                    format!("is too long: {} > {}", url.len(), PROJECT_URL_MAX)
+                )]));
+            }
+        }
+        
+        Ok(Self {
+            id: generate_id(PROJECT_ID_SIZE),
+            title,
+            description,
+            url,
+            created_at: Utc::now()
+        })
+    }
+
+    pub fn update(&mut self, title: String, description: String, url: Option<String>) {
+        self.title = title;
+        self.description = description;
+        self.url = url;
+    }
 }
