bf7cedaddd4d1c458645940e8f6831b8449d082b
JKearnsl <boyarshinovn@gmail.com>
2025-01-18 20:35:40 +0300

    update: remove unused code; now error are simpler

--- src/application/common/exceptions.rs
+++ src/application/common/exceptions.rs
@@ -1,44 +1,45 @@
 use std::collections::HashMap;
-
-use serde::Serialize;
-use crate::domain::exceptions::DomainError;
+use thiserror::Error;
 
 
-#[derive(Debug, Serialize, Clone)]
-pub enum ErrorContent {
-    Message(String),
-    Map(HashMap<String, String>),
+fn format_map(errors: &HashMap<String, String>) -> String {
+    let mut entries = errors.iter().collect::<Vec<_>>();
+    entries.sort_by(|a, b| a.0.cmp(b.0));
+
+    let formatted_entries: Vec<String> = entries
+        .iter()
+        .map(|(key, value)| format!("{}: {}", key, value))
+        .collect();
+
+    format!("[{}]", formatted_entries.join(", "))
 }
 
-impl From<DomainError> for ErrorContent {
-    fn from(error: DomainError) -> Self {
-        ErrorContent::Message(error.to_string())
-    }
-}
-
-impl From<&str> for ErrorContent {
-    fn from(s: &str) -> Self {
-        ErrorContent::Message(s.to_string())
-    }
-}
-
-impl From<String> for ErrorContent {
-    fn from(s: String) -> Self {
-        ErrorContent::Message(s)
-    }
-}
-
-impl From<HashMap<String, String>> for ErrorContent {
-    fn from(map: HashMap<String, String>) -> Self {
-        ErrorContent::Map(map)
-    }
-}
-
-#[derive(Debug, Serialize, Clone)]
+#[derive(Error, Debug, Clone)]
 pub enum ApplicationError {
-    InvalidData(ErrorContent),
-    NotFound(ErrorContent),
-    Conflict(ErrorContent),
-    Unauthorized(ErrorContent),
-    Forbidden(ErrorContent),
+    #[error("NotFound")]
+    NotFound,
+    #[error("ValidationError: {}", format_map(.0))]
+    ValidationError(HashMap<String, String>),
+    #[error("Unauthorized")]
+    Unauthorized,
+    #[error("Forbidden: You do not have permission to perform this action!")]
+    Forbidden,
+    #[error("UnexpectedError: {0}")]
+    UnexpectedError(String),
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_format_map() {
+        let mut map = HashMap::new();
+        map.insert("field1".to_string(), "error1".to_string());
+        map.insert("field2".to_string(), "error2".to_string());
+
+        let formatted = format_map(&map);
+        assert_eq!(formatted, "[field1: error1, field2: error2]");
+    }
 }
